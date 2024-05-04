91ae20323291992ee69ab890a637396993e4dfd0
Sunil Brijlall <brijsunil2@gmail.com>
2024-05-03 20:18:49 -0400

    Fixed issue where logo a tag will streach across the webpage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,17 +6,22 @@ import SearchBar from "./SearchBar";
 const Header = () => {
   return (
     <header>
-      <a className="site-logo" href="/">
-        <img
-          src={shoppingbag}
-          alt="Shopping bag logo for website"
-          width="32"
-          height="32"
-        />
-        <h1>Store E-Commerce</h1>
-      </a>
+      <div className="site-logo" >
+        <a href="/">
+          <img
+            src={shoppingbag}
+            alt="Shopping bag logo for website"
+            width="32"
+            height="32"
+          />
+          <h1>Store E-Commerce</h1>
+        </a>
+      </div>
+
       <div className="header-right-items">
-        <button className="cart-btn"><FaShoppingCart /> Cart</button>
+        <button className="cart-btn">
+          <FaShoppingCart /> Cart
+        </button>
       </div>
       <SearchBar></SearchBar>
     </header>
