c186fcba2e6f57a2617e6d8f09cbbd900ac43228
Sunil Brijlall <brijsunil2@gmail.com>
2024-05-05 18:19:15 -0400

    Implement api call functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import "./styles/App.css";
+import { useState, useEffect, useRef } from "react";
 import Header from "./components/Header";
 import FiltersSection from "./components/FiltersSection";
 import ProductsSection from "./components/ProductsSection";
+import { getProducts } from "./util/storeAPIFunc";
 
 function App() {
+  const [products, setProducts] = useState({});
+
+  useEffect(() => {
+    getProducts().then(res => setProducts(res))
+  }, [setProducts, getProducts])
+
   return (<>
     <Header/>
     <div className="body-container">
---
+++ frontend/src/util/storeAPIFunc.js
@@ -0,0 +1,19 @@
+const APIURL = "https://fakestoreapi.com"
+
+const getProducts = async (limit = null) => {
+  const req = await fetch(`${APIURL}/products${limit ? "" : `?${limit}`}`);
+  return await req.json();
+};
+
+const getCategories = async () => {
+  const req = await fetch(`${APIURL}/products/categories`);
+  return await req.json();
+}
+
+const getProductsByCategory = async (category, limit = null) => {
+  const req = await fetch(`${APIURL}/products/category/${category}/${limit ? "" : `?${limit}`}`);
+  return await req.json();
+}
+
+
+export { getProducts, getCategories, getProductsByCategory };
