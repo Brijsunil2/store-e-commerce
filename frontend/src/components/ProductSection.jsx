fe31a950d82152082e3043522e91fbba06e3b6e0
Sunil Brijlall <brijsunil2@gmail.com>
2024-05-05 21:15:05 -0400

    Add styles for ProductSection and ProductsSections for smaller screens

--- frontend/src/components/ProductSection.jsx
+++ frontend/src/components/ProductSection.jsx
@@ -1,14 +1,30 @@
 import "../styles/ProductSection.css";
 
 const ProductSection = ({ product }) => {
+  const handleAddToCart = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div className="productsection-container">
       <div className="product-img-container">
         <img src={product.image}></img>
       </div>
-      <h4 className="product-title">{product.title}</h4>
-      <h3 className="product-price">${parseFloat(product.price).toFixed(2)}</h3>
-      <button type="button">+ Add to Cart</button>
+      <div>
+        <h4 className="product-title">{product.title}</h4>
+        <h3 className="product-price">
+          ${parseFloat(product.price).toFixed(2)}
+        </h3>
+        <button
+          className="addtocart-btn"
+          key={product.id}
+          value={product.id}
+          type="button"
+          onClick={handleAddToCart}
+        >
+          + Add to Cart
+        </button>
+      </div>
     </div>
   );
 };
