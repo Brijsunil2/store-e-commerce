5a9126ea68057370446ee6780c9db5bd2962ccce
Sunil Brijlall <brijsunil2@gmail.com>
2024-05-24 20:28:16 -0400

    Impliment cart popup animation

--- frontend/src/components/CartPopup.jsx
+++ frontend/src/components/CartPopup.jsx
@@ -3,17 +3,18 @@ import { IoMdClose } from "react-icons/io";
 
 const CartPopup = ({ popup, setPopup }) => {
   return (
-    <>
-      {popup && (
-        <div className={`cart-popup-right ${popup && "cart-popup-show"}`}>
-          <div className="cart-popup-header">
-            <h3>Shopping Cart</h3>
-            <div className="close-popup" onClick={() => setPopup(false)}><IoMdClose /></div>
-          </div>
-          
+    <div className={`cart-popup-right ${popup && "cart-popup-active"}`}>
+      <div className="cart-popup-header">
+        <h3>Shopping Cart</h3>
+        <div className="close-popup" onClick={() => setPopup(false)}>
+          <IoMdClose />
         </div>
-      )}
-    </>
+      </div>
+      <div className="cart-popup-body"></div>
+      <div className="cart-popup-footer">
+        <button className="checkout-btn">Proceed to Checkout</button>
+      </div>
+    </div>
   );
 };
 
